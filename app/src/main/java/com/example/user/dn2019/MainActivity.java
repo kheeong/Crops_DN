10e3dd2faaa753f56040202f19bb2f12b48600d1
kheeong <42634274+kheeong@users.noreply.github.com>
2019-08-12 21:40:41 -0700

    V2

--- app/src/main/java/com/example/user/dn2019/MainActivity.java
+++ app/src/main/java/com/example/user/dn2019/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.dn2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText username = findViewById(R.id.Username);
+        final EditText password = findViewById(R.id.Password);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user == "one" && pass == "1"){
+                    Intent intent = new Intent(MainActivity.this,Main_page.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getBaseContext(),"Incorrect Password or Username",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/user/dn2019/Main_page.java
@@ -0,0 +1,12 @@
+package com.example.user.dn2019;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main_page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
