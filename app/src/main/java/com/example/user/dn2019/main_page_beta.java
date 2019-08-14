ae3636201aed8e1e172cf9ec554d13bbc2bf1ff1
unknownwyj <54095369+unknownwyj@users.noreply.github.com>
2019-08-14 17:49:17 +0800

    fail
    idk

---
+++ app/src/main/java/com/example/user/dn2019/main_page_beta.java
@@ -0,0 +1,45 @@
+package com.example.user.dn2019;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class main_page_beta extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.Price:
+                    mTextMessage.setText(R.string.Price);
+                    return true;
+                case R.id.Market:
+                    mTextMessage.setText(R.string.refresh);
+                    return true;
+                case R.id.navigation_homee:
+                    mTextMessage.setText(R.string.refresh);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page_beta);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
